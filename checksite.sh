cae24d15cbfeac9967e61503abac7dc7ff104ef1
Ismail Zahee <m3talloid@gmail.com>
2016-10-23 17:42:55 +0800

    small fixes

--- checksite.sh
+++ checksite.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-if curl -s --head  --request GET https://ebanking.bankofmaldives.com.mv/ --max-time 10 | grep "199 OK" > /dev/null;
+if curl -s --head  --request GET https://ebanking.bankofmaldives.com.mv/ --max-time 10 | grep "200 OK" > /dev/null;
 then
    say "Bank site is now up"
 else
    say "Still its down"
 fi
+# */2 * * * * cd /Users/xahy/myscripts && ./checksite.sh
